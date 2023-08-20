a2df06eb7f0d95cc3eb2c09c4ab5d716371599da
gymnast86 <stock.joann@gmail.com>
2023-08-20 11:04:14 -0700

    add starter code to sshdrando.py

--- sshdrando.py
+++ sshdrando.py
@@ -1,4 +1,12 @@
 from patches.allpatchhandler import AllPatchHandler
+from logic.generate import generate
+
+worlds = generate()
+
+for location in worlds[0].location_table.values():
+    item_name = f"{location.current_item}"
+    location_name = location.name
+    # Patch item into location
 
 patchHandler = AllPatchHandler()
 patchHandler.do_all_patches()
