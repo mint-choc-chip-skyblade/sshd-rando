207db1a06082d04e5c49d9335665aec06a73e8b6
gymnast86 <stock.joann@gmail.com>
2023-09-22 00:12:39 -0700

    account for plando'd goal locations

--- logic/world.py
+++ logic/world.py
@@ -384,12 +384,31 @@ class World:
                             dungeon.starting_entrance = exit_
 
     def choose_required_dungeons(self):
+        num_required_dungeons = self.setting("required_dungeons").value_as_number()
+
         dungeons = list(self.dungeons.values())
         dungeons.remove(self.dungeons["Sky Keep"])
+
+        # Remove any dungeons which have non-major items plandomized to their goal locations
+        # Also force require any dungeons which have a major item plandomized to their goal locations
+        for dungeon in dungeons.copy():
+            if any([True for loc in dungeon.goal_locations if not loc.is_empty() and not loc.current_item.is_major_item]):
+                dungeons.remove(dungeon)
+            elif any([True for loc in dungeon.goal_locations if not loc.is_empty() and loc.current_item.is_major_item]):
+                dungeon.required = True
+                num_required_dungeons -= 1
+                if num_required_dungeons < 0 and self.setting("empty_unrequired_dungeons") == "on":
+                    raise WrongInfoError("There are more major items plandomized at the end of dungeons than there are required dungeons. Please remove some plandomized major items at the end of dungeons")
+                logging.getLogger("").debug(f"Chose {dungeons} as required dungeon")
+
+        if num_required_dungeons > len(dungeons):
+            raise WrongInfoError("Not enough free goal locations to satisfy required dungeons. Please remove junk that has been plandomized at the end of dungeons")
+
         random.shuffle(dungeons)
-        for i in range(self.setting("required_dungeons").value_as_number()):
-            dungeons[i].required = True
-            logging.getLogger("").debug(f"Chose {dungeons[i]} as required dungeon")
+        for i in range(num_required_dungeons):
+            dungeon = dungeons.pop()
+            dungeon.required = True
+            logging.getLogger("").debug(f"Chose {dungeon} as required dungeon")
 
     def set_nonprogress_locations(self):
         # Set excluded locations as non-progress
