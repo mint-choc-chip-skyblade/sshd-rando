8f97f96fd0c23bb9ea8f05139876bca5968b144c
CovenEsme <84377742+CovenEsme@users.noreply.github.com>
2023-09-24 01:15:59 +0100

    Remove heetle from robots hands after check

--- asm/patches/always-spawn-technoblins-in-desert.asm
+++ asm/patches/always-spawn-technoblins-in-desert.asm
@@ -5,3 +5,7 @@ b 0x082e3b84
 ; Always spawn bokos near ToT gossip stone (not yet used in rando)
 .offset 0x082e3c24
 nop
+
+; Remove hook beetle from robot's hands after giving the item
+.offset 0x085759c8
+nop
