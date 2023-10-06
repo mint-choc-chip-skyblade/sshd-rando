2c76cde1a0dbda53a319149d5ed0d6450e057937
CovenEsme <84377742+CovenEsme@users.noreply.github.com>
2023-10-06 12:33:03 +0100

    Swap sun and moon for travelling night

--- asm/patches/fix-day-night.asm
+++ asm/patches/fix-day-night.asm
@@ -18,6 +18,24 @@ b.ne 0x08ba547c ; daytime
 b 0x08ba5474 ; nighttime
 
 
+; Show moon instead of sun when the night flag is set
+; 
+; Show moon
+; onlyif natural_night_connections == off
+.offset 0x08ec20b0
+mov w8, #10
+bl 0x0865a070 ; check day/night flag
+cmp w0, #0x1
+b.ne 0x08ec290c ; daytime = no moon
+
+; Show sun
+; onlyif natural_night_connections == off
+.offset 0x08ec3c04
+mov w8, #10
+bl 0x0865a070 ; check day/night flag
+tbnz w0, #0x0, 0x08ec3b60 ; night time = no sun
+
+
 ; Overwrite night sky filter
 ; .offset 0x08ba5474
 ; movz x8, #0x111, LSL #32
