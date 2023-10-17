9370dd8d28112d091fc45a5a9de22f4384a2e7ca
Madison Barry <madison.barry@hey.com>
2023-10-17 09:05:22 -0700

    Initial tracker commit (#76)
    Initialize a basic Godot 4.2 project with a startup autoload singleton for reading cmdline args

---
+++ tracker/systems/startup.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var ARGS := {
+	"seed": ""
+}
+
+func _ready():
+	for argument in OS.get_cmdline_user_args():
+		if argument.find("=") > -1:
+			var key_value = argument.split("=")
+			ARGS[key_value[0].lstrip("--")] = key_value[1]
+		else:
+			# Options without an argument will be present in the dictionary,
+			# with the value set to an empty string.
+			ARGS[argument.lstrip("--")] = ""
+	
+	print_debug("cmdline args: \n", ARGS)
