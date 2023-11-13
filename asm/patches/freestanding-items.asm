b7497985e15be1b16902a11835a1f57beb8f8885
CovenEsme <84377742+CovenEsme@users.noreply.github.com>
2023-11-13 20:32:59 +0000

    Remove some arrow related pickup textbox

--- asm/patches/freestanding-items.asm
+++ asm/patches/freestanding-items.asm
@@ -117,6 +117,10 @@ mov w2, #0xfe00 ; from 0xfc00 (sets bit 9 of param1)
 .offset 0x082d7b94 ; 0x71002d3b94
 mov w2, #0xfe00 ; from 0xfc00 (sets bit 9 of param1)
 
+; Some arrow thing?
+.offset 0x08a7d988 ; 0x7100a79988
+mov w2, #0xfe00 ; from 0xfc00 (sets bit 9 of param1)
+
 ; Lizalfos Tails
 .offset 0x083ac274 ; 0x71003a8274
 mov w2, #0xfe00 ; from 0xfc00 (sets bit 9 of param1)
