6c254fde6a255773210ef9fa03df215522c3e691
CovenEsme <84377742+CovenEsme@users.noreply.github.com>
2023-11-20 22:06:20 +0000

    Actually fix the ToT and inner FS birb statues

--- asm/patches/fix-bird-statues.asm
+++ asm/patches/fix-bird-statues.asm
@@ -4,8 +4,8 @@ mov x0, #0x0
 
 ; Change HD progression storyflag for inner FS bird statue
 .offset 0x093a2530 ; 0x710139e530
-.short 901 ; rando beaten FS storyflag
+.short -1 ; rando beaten FS storyflag
 
 ; Change HD progression storyflag for Temple of Time bird statue
 .offset 0x093a25c8 ; 0x710139e5c8
-.short 935 ; rando exit out of LMF for first time storyflag
+.short -1 ; rando exit out of LMF for first time storyflag
