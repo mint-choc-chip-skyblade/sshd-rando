c56253df4c0326b450a0cbda6328a2e2a91616b3
CovenEsme <84377742+CovenEsme@users.noreply.github.com>
2023-11-22 14:10:05 +0000

    Add about message box

--- gui/main.py
+++ gui/main.py
@@ -2,11 +2,11 @@ import sys
 
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QApplication, QMainWindow, QProgressDialog
+from PySide6.QtWidgets import QApplication, QMessageBox, QMainWindow, QProgressDialog
 
 from constants.randoconstants import VERSION
 from filepathconstants import ICON_PATH
-from gui.ui.ui_main import Ui_MainWindow
+from gui.ui.ui_main import Ui_main_window
 from gui.guithreads import RandomizationThread
 
 
@@ -16,7 +16,7 @@ class Main(QMainWindow):
 
         self.randomize_thread = RandomizationThread()
 
-        self.ui = Ui_MainWindow()
+        self.ui = Ui_main_window()
         self.ui.setupUi(self)
 
         self.setWindowTitle(
@@ -26,12 +26,13 @@ class Main(QMainWindow):
         self.setWindowIcon(QIcon(ICON_PATH.as_posix()))
 
         self.ui.randomize_button.clicked.connect(self.randomize)
+        self.ui.about_button.clicked.connect(self.about)
 
     def randomize(self):
         self.progress_dialog = QProgressDialog("Initializing", "", 0, 100, self)
         self.progress_dialog.setWindowTitle("Randomizing...")
         self.progress_dialog.setWindowModality(Qt.WindowModality.WindowModal)
-        self.progress_dialog.setCancelButton(None)
+        self.progress_dialog.setCancelButton(None)  # type: ignore
         self.progress_dialog.setValue(0)
         self.progress_dialog.setMinimumWidth(250)
         self.progress_dialog.setVisible(True)
@@ -45,6 +46,27 @@ class Main(QMainWindow):
         self.randomize_thread.setTerminationEnabled(True)
         self.randomize_thread.start()
         self.progress_dialog.exec()
+
+    def about(self):
+        self.about_dialog = QMessageBox(self)
+        self.about_dialog.setTextFormat(Qt.TextFormat.RichText)
+
+        about_text = f"""
+                        <b>The Legend of Zelda: Skyward Sword HD Randomizer</b><br>
+                        Version: {VERSION}<br><br>
+
+                        Created by:
+                        <a href=\"https://github.com/covenesme\">CovenEsme</a>,
+                        <a href=\"https://github.com/muzugalium\">Muzugalium</a>,
+                        <a href=\"https://github.com/gymnast86\">Gymnast86</a>, and
+                        <a href=\"https://github.com/tbpixel\">tbpixel</a><br><br>
+
+                        <a href=\"https://github.com/mint-choc-chip-skyblade/sshd-rando/issues\">Report issues</a>
+                        or view the 
+                        <a href=\"https://github.com/mint-choc-chip-skyblade/sshd-rando\">Source code</a>
+        """
+
+        self.about_dialog.about(self, "About", about_text)
 
 
 def start_gui():
--- gui/ui/ui_main.py
+++ gui/ui/ui_main.py
@@ -42,28 +42,64 @@ from PySide6.QtGui import (
 )
 from PySide6.QtWidgets import (
     QApplication,
+    QHBoxLayout,
     QMainWindow,
     QPushButton,
     QSizePolicy,
+    QSpacerItem,
+    QTabWidget,
     QVBoxLayout,
     QWidget,
 )
 
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        if not MainWindow.objectName():
-            MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
-        self.centralwidget = QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.verticalLayout_2 = QVBoxLayout(self.centralwidget)
+class Ui_main_window(object):
+    def setupUi(self, main_window):
+        if not main_window.objectName():
+            main_window.setObjectName("main_window")
+        main_window.resize(800, 600)
+        self.central_widget = QWidget(main_window)
+        self.central_widget.setObjectName("central_widget")
+        self.verticalLayout_2 = QVBoxLayout(self.central_widget)
         self.verticalLayout_2.setObjectName("verticalLayout_2")
-        self.verticalLayout = QVBoxLayout()
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.randomize_button = QPushButton(self.centralwidget)
+        self.tab_widget = QTabWidget(self.central_widget)
+        self.tab_widget.setObjectName("tab_widget")
+        self.tab = QWidget()
+        self.tab.setObjectName("tab")
+        self.tab_widget.addTab(self.tab, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName("tab_2")
+        self.tab_widget.addTab(self.tab_2, "")
+
+        self.verticalLayout_2.addWidget(self.tab_widget)
+
+        self.footer_layout = QHBoxLayout()
+        self.footer_layout.setObjectName("footer_layout")
+        self.about_button = QPushButton(self.central_widget)
+        self.about_button.setObjectName("about_button")
+
+        self.footer_layout.addWidget(self.about_button)
+
+        self.footer_left_hspacer = QSpacerItem(
+            40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum
+        )
+
+        self.footer_layout.addItem(self.footer_left_hspacer)
+
+        self.reset_button = QPushButton(self.central_widget)
+        self.reset_button.setObjectName("reset_button")
+
+        self.footer_layout.addWidget(self.reset_button)
+
+        self.footer_right_hspacer = QSpacerItem(
+            40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum
+        )
+
+        self.footer_layout.addItem(self.footer_right_hspacer)
+
+        self.randomize_button = QPushButton(self.central_widget)
         self.randomize_button.setObjectName("randomize_button")
-        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(
@@ -71,26 +107,40 @@ class Ui_MainWindow(object):
         )
         self.randomize_button.setSizePolicy(sizePolicy)
 
-        self.verticalLayout.addWidget(self.randomize_button)
+        self.footer_layout.addWidget(self.randomize_button)
 
-        self.verticalLayout_2.addLayout(self.verticalLayout)
+        self.verticalLayout_2.addLayout(self.footer_layout)
 
-        MainWindow.setCentralWidget(self.centralwidget)
+        main_window.setCentralWidget(self.central_widget)
 
-        self.retranslateUi(MainWindow)
+        self.retranslateUi(main_window)
 
-        QMetaObject.connectSlotsByName(MainWindow)
+        QMetaObject.connectSlotsByName(main_window)
 
     # setupUi
 
-    def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(
+    def retranslateUi(self, main_window):
+        main_window.setWindowTitle(
             QCoreApplication.translate(
-                "MainWindow", "The Legend of Zelda: Skyward Sword HD Randomizer", None
+                "main_window", "The Legend of Zelda: Skyward Sword HD Randomizer", None
             )
         )
+        self.tab_widget.setTabText(
+            self.tab_widget.indexOf(self.tab),
+            QCoreApplication.translate("main_window", "Tab 1", None),
+        )
+        self.tab_widget.setTabText(
+            self.tab_widget.indexOf(self.tab_2),
+            QCoreApplication.translate("main_window", "Tab 2", None),
+        )
+        self.about_button.setText(
+            QCoreApplication.translate("main_window", "About", None)
+        )
+        self.reset_button.setText(
+            QCoreApplication.translate("main_window", "Reset Settings to Default", None)
+        )
         self.randomize_button.setText(
-            QCoreApplication.translate("MainWindow", "Randomize", None)
+            QCoreApplication.translate("main_window", "Randomize", None)
         )
 
     # retranslateUi
