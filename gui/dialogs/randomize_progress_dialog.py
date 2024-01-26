411fca674e6dd1049cc4c588a6c679ae2c56d65b
CovenEsme <84377742+CovenEsme@users.noreply.github.com>
2024-01-26 16:34:35 +0000

    Add open output button to done dialog

--- gui/dialogs/randomize_progress_dialog.py
+++ gui/dialogs/randomize_progress_dialog.py
@@ -1,14 +1,19 @@
-from PySide6.QtCore import Qt
-from PySide6.QtGui import QCloseEvent, QKeyEvent, QIcon
+from PySide6.QtCore import Qt, QUrl, QSize
+from PySide6.QtGui import QCloseEvent, QDesktopServices, QKeyEvent, QIcon, QPixmap
 from PySide6.QtWidgets import QMessageBox, QProgressDialog
 
 from filepathconstants import ICON_PATH
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from gui.main import Main
+
 
 class RandomizerProgressDialog(QProgressDialog):
-    def __init__(self, parent):
-        QProgressDialog.__init__(self, parent)
-        self.parent = parent
+    def __init__(self, main: "Main"):
+        QProgressDialog.__init__(self, main)
+        self.main = main
         self.setWindowTitle("Randomizing...")
         self.setWindowModality(Qt.WindowModality.WindowModal)
         self.setLabelText("Initializing")
@@ -26,9 +31,29 @@ class RandomizerProgressDialog(QProgressDialog):
 
         if self.should_show_done:
             done_dialog = QMessageBox(self)
-            done_dialog.about(
-                self, "Randomization Completed", "Seed successfully generated!"
+            done_dialog.setWindowTitle("Randomization Completed")
+            done_dialog.setText("Seed successfully generated!")
+
+            open_output_button = done_dialog.addButton(
+                "Open Output Folder", QMessageBox.ButtonRole.NoRole
             )
+            open_output_button.clicked.connect(self.open_output_folder)
+
+            done_dialog.addButton("OK", QMessageBox.ButtonRole.NoRole)
+
+            done_dialog.setWindowIcon(QIcon(ICON_PATH.as_posix()))
+            icon_pixmap = QPixmap(ICON_PATH.as_posix()).scaled(
+                QSize(80, 80),
+                Qt.AspectRatioMode.IgnoreAspectRatio,
+                Qt.TransformationMode.SmoothTransformation,
+            )
+            done_dialog.setIconPixmap(icon_pixmap)
+            done_dialog.exec()
+
+    def open_output_folder(self):
+        QDesktopServices.openUrl(
+            QUrl(self.main.config.output_dir.as_posix(), QUrl.ParsingMode.TolerantMode)
+        )
 
     def keyPressEvent(self, e: QKeyEvent):
         # Prevent the escape key from closing the progress dialog
@@ -38,4 +63,4 @@ class RandomizerProgressDialog(QProgressDialog):
     def closeEvent(self, e: QCloseEvent):
         self.should_show_done = False
         # Close the entire program if the progress dialog is closed
-        self.parent.close()
+        self.main.close()
