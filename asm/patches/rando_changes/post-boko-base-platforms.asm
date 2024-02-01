0df18a8d25983b97e8a894f1c5fd8a91591a4767
gymnast86 <stock.joann@gmail.com>
2024-01-31 23:20:19 -0800

    fix lava platforms not spawning

--- asm/patches/rando_changes/post-boko-base-platforms.asm
+++ asm/patches/rando_changes/post-boko-base-platforms.asm
@@ -2,7 +2,7 @@
 ; Boko Base has been completed
 .offset 0x7100e90ab8
 mov x19, x0 ; code afterwards expects pointer in x19
-mov w8, #29
+mov w8, #30
 bl additions_jumptable
 cmp x0, #1
 b.ne 0x7100e90ba8
