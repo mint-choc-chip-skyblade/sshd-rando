197be4df7cee9a80dfe3cbe3bb617b73a1996cd1
gymnast86 <stock.joann@gmail.com>
2024-02-25 17:53:56 -0800

    fix startflags for important items

--- patches/conditionalpatchhandler.py
+++ patches/conditionalpatchhandler.py
@@ -21,6 +21,9 @@ class ConditionalPatchHandler:
                 return all([self.evaluate_statements(arg) for arg in statement.args])
             elif statement.type == RequirementType.NOT:
                 return not ([self.evaluate_statements(arg) for arg in statement.args])
+            elif statement.type == RequirementType.ITEM:
+                item = statement.args[0]
+                return self.world.starting_item_pool[item] > 0
             elif statement.type == RequirementType.COUNT:
                 count = statement.args[0]
                 item = statement.args[1]
