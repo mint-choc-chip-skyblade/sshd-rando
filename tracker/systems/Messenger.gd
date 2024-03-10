aaa8a9deea4f82a7ca576992e0d00b491a164517
Madison Barry <madison.barry@hey.com>
2024-03-09 16:07:17 -0800

    Basic toggleable checks with emitted event

--- tracker/systems/Messenger.gd
+++ tracker/systems/Messenger.gd
@@ -1,8 +1,19 @@
 extends Node
 
+# Map Transition Zooms
 enum Zoom {
 	IN = 1,
 	OUT = -1
 }
-
 signal zoom(direction: Zoom, scene: PackedScene)
+
+# Checks
+signal check_toggled(key: String, is_checked: bool)
+
+
+func _ready():
+	check_toggled.connect(_on_check_toggled)
+
+
+func _on_check_toggled(key: String, is_checked: bool) -> void:
+	print_debug(key, is_checked)
