ee84f31c3d9f2b4c10b04972e6515cb7fca59543
Xander DiGiovanni <37936141+YourAverageLink@users.noreply.github.com>
2024-06-02 15:30:32 -0500

    tooltip fix for real this time?

--- gui/components/tracker_toggle_st_button.py
+++ gui/components/tracker_toggle_st_button.py
@@ -2,6 +2,7 @@ from PySide6.QtWidgets import QPushButton, QToolTip
 from PySide6.QtGui import QCursor, QMouseEvent
 from PySide6 import QtCore
 from PySide6.QtCore import Signal, QPoint
+from constants.guiconstants import TRACKER_TOOLTIP_STYLESHEET
 
 
 class TrackerToggleSTButton(QPushButton):
@@ -14,6 +15,7 @@ class TrackerToggleSTButton(QPushButton):
         self.setText("Enable Sphere Tracking")
         self.setCursor(QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
         self.setMouseTracking(True)
+        self.setStyleSheet(TRACKER_TOOLTIP_STYLESHEET)
 
     def mouseMoveEvent(self, ev: QMouseEvent) -> None:
         coords = self.mapToGlobal(QPoint(0, 0)) + QPoint(100, int(self.height() / 4))
