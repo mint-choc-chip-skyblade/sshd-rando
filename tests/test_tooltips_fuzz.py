16a37cfd72e9e93c65686577288939ce0b5b4d87
robojumper <robojumper@gmail.com>
2024-06-14 15:21:59 +0200

    Format

--- tests/test_tooltips_fuzz.py
+++ tests/test_tooltips_fuzz.py
@@ -31,9 +31,7 @@ def default_tracker_settings() -> SettingMap:
     setting_map = SettingMap()
     setting_map.starting_inventory = get_default_setting("starting_inventory")
     setting_map.excluded_locations = get_default_setting("excluded_locations")
-    setting_map.excluded_hint_locations = get_default_setting(
-        "excluded_hint_locations"
-    )
+    setting_map.excluded_hint_locations = get_default_setting("excluded_hint_locations")
     setting_map.mixed_entrance_pools = get_default_setting("mixed_entrance_pools")
     all_settings_info = get_all_settings_info()
     for setting, info in all_settings_info.items():
