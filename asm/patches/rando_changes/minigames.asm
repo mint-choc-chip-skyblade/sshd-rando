2032933f433fc5a2bbab044832d99d28a41f777c
CovenEsme <84377742+CovenEsme@users.noreply.github.com>
2025-02-25 22:18:12 +0000

    Add minigame difficulty setting and implement it for pumpkin archery

--- asm/patches/rando_changes/minigames.asm
+++ asm/patches/rando_changes/minigames.asm
@@ -8,3 +8,33 @@ mov w21, w1
 ; End Pumpkin Archery Early by hitting the bell
 .offset 0x710072884c
 bl 0x7100659ad0
+
+
+; Minigame win condition changes
+
+; ; Pumpkin Archery Guaranteed Win
+; onlyif minigame_difficulty == guaranteed_win
+.offset 0x7100604724
+b 0x7100604744
+
+; Pumpkin Archery Easy
+; onlyif minigame_difficulty == easy
+.offset 0x7100604724
+cmp w8, #399
+; onlyif minigame_difficulty == easy
+.offset 0x710060472c
+cmp w8, #149
+; onlyif minigame_difficulty == easy
+.offset 0x7100604738
+cmp w8, #9
+
+; Pumpkin Archery Hard
+; onlyif minigame_difficulty == hard
+.offset 0x7100604724
+cmp w8, #799
+; onlyif minigame_difficulty == hard
+.offset 0x710060472c
+cmp w8, #599
+; onlyif minigame_difficulty == hard
+.offset 0x7100604738
+cmp w8, #399
