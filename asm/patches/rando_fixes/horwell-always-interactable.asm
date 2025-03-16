5627fe03a849035c769c96936d437e0ea84faf20
CovenEsme <84377742+CovenEsme@users.noreply.github.com>
2025-03-16 21:03:59 +0000

    Control Horwell and Mia spawning

--- asm/patches/rando_fixes/horwell-always-interactable.asm
+++ asm/patches/rando_fixes/horwell-always-interactable.asm
@@ -2,3 +2,14 @@
 .offset 0x7100661b30
 mov w8, #31
 bl additions_jumptable
+
+; Control if Horwell spawns
+.offset 0x7100660a10
+mov w8, #81
+bl additions_jumptable
+
+
+; Control if Mia the remlit spawn
+.offset 0x710041a744
+mov w8, #82
+bl additions_jumptable
