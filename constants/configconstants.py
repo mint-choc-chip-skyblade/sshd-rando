832c9c1fab560ef951a4dc5cc0f6f9059396a455
CovenEsme <84377742+CovenEsme@users.noreply.github.com>
2025-05-06 21:06:59 +0100

    Update shared closet check names

--- constants/configconstants.py
+++ constants/configconstants.py
@@ -174,11 +174,13 @@ LOCATION_ALIASES = {
     "Central Skyloft - Crystal in Loftwing Prison": "Central Skyloft - Crystal in Loftwing Prison after Waterfall Cave",
     "Central Skyloft - Stamina Fruit near Bazaar": "Central Skyloft - Stamina Fruit between Stairs near the Bazaar",
     "Central Skyloft - Stamina Fruit near Wryna's House": "Central Skyloft - Stamina Fruit near Kukiel's House",
-    "Central Skyloft - Wryna's Closet": "Central Skyloft - Kukiel's Closet",
+    "Central Skyloft - Wryna's Closet": "Central Skyloft - Kukiel's Family Closet",
     "Skyloft Village - Stamina Fruit above Mallara's House": "Skyloft Village - Stamina Fruit above Pipit's House",
     "Skyloft Village - Clean Mallara's House": "Skyloft Village - Clean Pipit's House",
-    "Skyloft Village - Mallara's Closet": "Skyloft Village - Pipit's Closet",
+    "Skyloft Village - Mallara's Closet": "Skyloft Village - Pipit and Mallara's Closet",
     "Skyloft Village - Luv and Bertie's Closet": "Skyloft Village - Bertie and Luv's Closet",
+    "Skyloft Village - Gondo's Closet": "Skyloft Village - Gondo and Gebra's Closet",
+    "Skyloft Village - Rupin's Closet": "Skyloft Village - Rupin and Goselle's Closet",
 }
 
 TRACKER_NOTE_EVENTS = {
