d839f5f50ea3d4462802c2a77b0d12c8916b27b6
CovenEsme <84377742+CovenEsme@users.noreply.github.com>
2025-05-26 12:49:20 +0100

    Fix freestanding Dusk Relic scale

--- asm/additions/rust-additions/src/item.rs
+++ asm/additions/rust-additions/src/item.rs
@@ -1420,8 +1420,8 @@ pub fn get_custom_freestanding_item_scale() -> f32 {
 
         return match (*item_actor).final_determined_itemid {
             40 => 1.5, // 5 Bombs
-            // Tumbleweed, Ancient Flower, Blue Bird Feather, Goddess Plume
-            60 | 163 | 166 | 174 | 176 => 2.0,
+            // Tumbleweed, Ancient Flower, Dusk Relic, Blue Bird Feather, Goddess Plume
+            60 | 163 | 166 | 168 | 174 | 176 => 2.0,
             _ => 1.0,
         };
     }
